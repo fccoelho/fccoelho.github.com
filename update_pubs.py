7df3a0d6382135115986da392b02fd83faf7b7ca
Flavio Codeço Coelho <fccoelho@gmail.com>
2023-10-16 17:44:19 -0300

    Dowloading Scholar publications

--- update_pubs.py
+++ update_pubs.py
@@ -1,6 +1,22 @@
 from scholarly import scholarly
 import pandas as pd
 
-def publist():
+def get_author():
     search_query = scholarly.search_author('Flávio Codeço Coelho')
-    while True:
+    author = next(search_query)
+    return author
+
+def  get_pubs(author):
+    pubs = []
+    author = scholarly.fill(author)
+    for pub in author['publications']:
+        # pub = scholarly.fill(pub)
+        pubs.append(pub)
+    return pubs
+
+if __name__== "__main__":
+    author = get_author()
+    dfpubs = pd.DataFrame(get_pubs(author))
+    print (dfpubs)
+    # pubs = pd.DataFrame(author.publications)
+    # pubs.to_csv('publications.csv', index=False)
