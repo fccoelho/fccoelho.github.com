a79691729e5306e01ef39778bbb745b22f117fdc
Flávio Codeço Coelho <fccoelho@gmail.com>
2024-11-23 10:16:14 -0300

    updated publication list

--- update_pubs.py
+++ update_pubs.py
@@ -2,6 +2,7 @@ from scholarly import scholarly
 import pandas as pd
 import json
 import os
+import datetime
 from tqdm import tqdm
 import pickle
 from icecream import ic
@@ -22,10 +23,18 @@ def  get_pubs(author):
     return pubs
 
 def save_bib(refs):
-
-    refs = sorted([r for r in refs if 'pub_year' in r], key=lambda x: x['pub_year'], reverse=True)
+    """
+    Save bibfile
+    """
+    refs_sorted = sorted([r for r in refs if 'pub_year' in r], key=lambda x: x['pub_year'], reverse=True)
+    if len(refs) != len(refs_sorted):
+        refs_missing = [r for r in refs if r not in refs_sorted]
+        print(f"Missing {len(refs_missing)} references in pub list, due to missing pub_year field. Adding them to the end.")
+        for ref in refs_missing:
+            ref['pub_year'] = "--"
+            refs_sorted.append(ref)
     with open('refs.bib', 'w') as bibfile:
-        for i, ref in enumerate(tqdm(refs)):
+        for i, ref in enumerate(tqdm(refs_sorted)):
             if 'abstract' in ref:
                 del ref['abstract']
             if ('journal' not in ref):
@@ -66,10 +75,18 @@ def gen_ref_list():
 if __name__== "__main__":
     author = get_author()
     if os.path.exists('pubs.pkl'):
-        pubs = pickle.load(open('pubs.pkl', 'rb'))
+        fstat = os.stat('pubs.pkl')
+        cdate = datetime.datetime.fromtimestamp(fstat.st_ctime)
+        if cdate > datetime.datetime.now() - datetime.timedelta(days=1):
+            pubs = pickle.load(open('pubs.pkl', 'rb'))
+        else:
+            print('Updating publication list')
+            pubs = get_pubs(author)
+            pickle.dump(pubs, open(f'pubs.pkl', 'wb'))
     else:
+        print('Updating publication list')
         pubs = get_pubs(author)
-        pickle.dump(pubs, open('pubs.pkl', 'wb'))
+        pickle.dump(pubs, open(f'pubs.pkl', 'wb'))
     save_bib([p['bib'] for p in pubs])
     gen_ref_list()
     dfpubs = pd.DataFrame(pubs)
